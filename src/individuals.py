fixed culture! issue with normalization

src/individuals.py
@@ -22,6 +22,7 @@ class Individual:
         self.carbon_intensive_list = individual_params["carbon_emissions"]
         self.culture_momentum = individual_params["culture_momentum"]
         self.discount_list = individual_params["discount_list"]
+        self.sum_discount_list = sum(self.discount_list)
         self.carbon_price_state = individual_params["carbon_price_state"]
         self.information_provision_state = individual_params["information_provision_state"]
         self.compression_factor = individual_params["compression_factor"]
@@ -73,13 +74,8 @@ class Individual:
         self.av_behaviour_list.append(self.av_behaviour)#what if its attraction instead?
 
     def calc_culture(self) -> float:
-        weighted_sum_behaviours = 0
-
-        for i in range(len(self.av_behaviour_list)):
-            weighted_sum_behaviours += self.discount_list[i]*self.av_behaviour_list[i]
-        normalized_culture = weighted_sum_behaviours/len(self.av_behaviour_list)
-        #print(len(self.av_behaviour_list))
-        return normalized_culture
+        indiv_cul = np.matmul(self.discount_list, self.av_behaviour_list)/self.sum_discount_list
+        return indiv_cul
 
     def update_values(self):
         self.values = self.attracts - self.thresholds

src/network.py
@@ -253,28 +253,22 @@ class Network:
         """
         Calc the individual culture of the attraction matrix for homophilly
         """
-        weighted_sum_behaviours = 0
-
         av_behaviour = attracts.sum()/attracts.shape
-
         av_behaviour_list = [av_behaviour]*self.culture_momentum
-
-        for i in range(len(av_behaviour_list)):
-            weighted_sum_behaviours += self.discount_list[i]*av_behaviour_list[i]
-        normalized_culture = weighted_sum_behaviours/len(av_behaviour_list)
-        #print(normalized_culture)
-        return normalized_culture[0]
+        #### HERE I HAVE THE CORRECT LIST OF AV BEAHVIOUR
+        indiv_cul = np.matmul(self.discount_list, av_behaviour_list)/sum(self.discount_list)
+        return indiv_cul
 
     def quick_calc_culture(self,attract_matrix):
         """
         Create culture list from the attraction matrix for homophilly
         """
+
         cul_list = []
         for i in range(len(attract_matrix)):
             cul_list.append(self.quick_indiv_calc_culture(attract_matrix[i]))
 
-        #print(np.mean(cul_list))
-        #print("non circ cul_list: ",cul_list)
+        #print("cul_list = ", np.mean(cul_list))
         return cul_list
 
 